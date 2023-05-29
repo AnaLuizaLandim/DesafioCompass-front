dfcb3aed60df7d44056fb3ad0b4db6c01b6dd0fd
Ana Luiza <analuizalandim9@gmail.com>
2023-05-29 16:10:48 -0300

    Feat: adjustment

--- challengeback/src/service/user-service.ts
+++ challengeback/src/service/user-service.ts
@@ -175,30 +175,3 @@ export function getUserById(id: number) {
   });
 }
 
-export default async function CreateTableUsers() {
-    openDbLocal().then(db=>{db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            user TEXT,
-            birthdate DATE,
-            email TEXT,
-            password TEXT,
-            profile_photo TEXT
-    )
-  `)
-})
-}
-
-export async function InsertUser(user: any) {
-    openDbLocal().then(db => {
-        db.run(
-          `
-          INSERT INTO users (id, name, user, birthdate, email, password, profile_photo)
-          VALUES (?, ?, ?, ?, ?, ?, ?)
-          `,
-          [user.id, user.name, user.user, user.birthdate, user.email, user.password, user.profile_photo]
-        );
-      });
-    }
-  
